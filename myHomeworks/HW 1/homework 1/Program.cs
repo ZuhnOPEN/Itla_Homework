32917c093d04a889bc31fe3d9c8f4051844ee647
Randolf Vladimir Martinez Beltre <20241725@itla.edu.do>
2025-05-24 12:01:55 -0400

    updated routes

---
+++ myHomeworks/HW 1/homework 1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+int valor1 = 10;
+double valor2 = 12.42;
+float valor3 = 2.4421321F;
+string lettme = "Well i mean its all right like";
+
+char letter = 'A';
+
+bool imReal = false;
+bool imFalse = true;
+
+//Numbers
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+
+// Letters/strings
+Console.WriteLine(lettme);
+Console.WriteLine(letter);
+
+//Booleans
+Console.WriteLine(imReal);
+Console.WriteLine(imFalse);
+
+
+/*float numbers = 10152466.25F;
+
+byte smallStorage = 5;
+
+float res = numbers + smallStorage;
+
+Console.WriteLine(res);
+*/
+
+Console.WriteLine(DateTime.Now);
