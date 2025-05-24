753ebc875db1bf751361878f21b43acf75c4b41f
Randolf Vladimir Martinez Beltre <20241725@itla.edu.do>
2025-05-24 00:44:18 -0400

    add myHomeworks

---
+++ myHomeworks/HW 1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿/*float numbers = 10152466.25F;
+
+byte smallStorage = 5;
+
+float res = numbers + smallStorage;
+
+Console.WriteLine(res);
+*/
+
+Console.WriteLine(DateTime.Now);
---
+++ myHomeworks/HW 1/Prog 1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+    
+        int valor1 = 10;
+        double valor2 = 12.42;
+        float valor3 = 2.4421321F;
+        string lettme = "Well i mean its all right like";
+        
+        char letter = 'A';
+        
+        bool imReal = false;
+        bool imFalse = true;
+       
+    //Numbers
+        Console.WriteLine(valor1);
+        Console.WriteLine(valor2);
+        Console.WriteLine(valor3);
+
+// Letters/strings
+    Console.WriteLine(lettme);
+        Console.WriteLine(letter);
+       
+    //Booleans
+        Console.WriteLine(imReal);
+        Console.WriteLine(imFalse);
+
+    }
+}
